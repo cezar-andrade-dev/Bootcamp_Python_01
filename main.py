bca1485a42b3dc7ec0d7add4b8cfd662dea64262
cezar-andrade-dev <cezar.andrade.dev@gmail.com>
2025-07-21 15:44:52 -0300

    Código desafio 1.

--- main.py
+++ main.py
@@ -1,6 +1,22 @@
-var1 = input("Digite o seu nome: ")
-var2 = int(input("Digite seu salário: "))
+# desafio 1 - Cálculo de KPI do bônus: 1000 + salário * bônus
 
-bonus = 1
+
+#solicitando o nome do usuário
+nome_usuario = input("Digite o seu nome: ")
+
+#solicitando o valor do salário
+sal_usuario = float(input("Digite o valor do seu salário: "))
+
+#Solicitando o valor do Bônus
+bonus_usuario = float(input("Digite o valor do Bônus recebido: "))
+
+#cálculo do bônus final
+CONST_BONUS = 1000
+valor_do_bonus = CONST_BONUS + sal_usuario*bonus_usuario
+
+#Imprimindo os valores
+
+print(f"O usuário {nome_usuario} possui o bônus de {valor_do_bonus}.")
+
 
 
